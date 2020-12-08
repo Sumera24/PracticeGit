7a7c978a6b0ae4de9b5aa2739912c1828339f0c5
Sumera24 <sumeraqz@gmail.com>
2020-12-08 11:32:10 -0500

    New Laptop

--- src/module1/Day2.java
+++ src/module1/Day2.java
@@ -1,11 +1,12 @@
 package module1;
 
-public class Day_1 {
+public class Day2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sumera Quazi");
 		System.out.println("Sumera Quazi");
+
 
 	}
 
--- src/module2/Day3.java
+++ src/module2/Day3.java
@@ -1,11 +1,12 @@
-package module1;
+package module2;
 
-public class Day_1 {
+public class Day3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sumera Quazi");
 		System.out.println("Sumera Quazi");
+
 
 	}
 
